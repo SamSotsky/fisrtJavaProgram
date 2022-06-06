9bc216d417df2c6bf3ee6b68a95a36a19ec647ad
SamSotsky <sam.sotsky01@gmail.com>
2022-06-06 11:29:12 -0700

    First Java

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("My name is Ronald Jones");
+        System.out.println("I am 100 years years oldness");
+        System.out.println("My hometown is yeah probably yuh");
+	}
+}
